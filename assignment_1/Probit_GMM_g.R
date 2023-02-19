cafc2340b5a47004aedbbaddf916212416793760
Nicolas <n.goeller@zeppelin-university.net>
2023-02-19 14:56:43 +0100

    Completed GMM gradient function

--- assignment_1/Probit_GMM_g.R
+++ assignment_1/Probit_GMM_g.R
@@ -3,9 +3,13 @@ Probit_GMM_g <- function(y,x,par) {
   n = length(y) 
   k = length(par)
 	
-  ...
-
-	f = ...
-
+  Phi = pnorm(x %*% par)
+  phi = dnorm(x %*% par)
+  W_hat = diag(k)
+  
+	f = -2*((1/n)*((t(matrix(rep(phi,k),nrow = n)) * t(x)) %*% x) %*% W_hat %*% ((1/n)*t(x)%*%(y - Phi)))
+#                 t(nxk)                        ??t()     nxk    kxk              t(nxk)  nx1
+#                         kxk                                       kxk                   kx1
+	
 	return(f)
 }
