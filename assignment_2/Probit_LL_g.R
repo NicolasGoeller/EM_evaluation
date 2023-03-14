87462f34a151077e98b94969168413beab3c8137
Nicolas <n.goeller@zeppelin-university.net>
2023-03-14 17:39:46 +0100

    Renamed file for d

--- assignment_2/Probit_LL_g.R
+++ assignment_2/Probit_LL_g.R
@@ -1,7 +1,7 @@
 Probit_LL_g <- function (y,x,par) {
   
   n = length(y) 
-  k = length(par)
+  #k = length(par)
   
   Phi = pnorm(x %*% par) # generate cdf
   phi = dnorm(x %*% par) # generate pdf
